1f2232fca08790a9f4799d33a0172aa221a7c610
TianTepex <149849417+TianTepex@users.noreply.github.com>
2024-05-06 11:03:47 -0500

    Add files via upload

---
+++ 0. ejecucionProyecto/variables.php
@@ -0,0 +1,20 @@
+<?php
+#variable numerica
+
+$numero=9;
+echo"esta es una variable numero: $numero <br>"
+var_dump ($numero);
+echo"<br><br>";
+
+
+#variable boleana
+
+$boleana= true;
+echo"esta es una variable buleana: $boleana <br>"
+var_dump ($boleana);
+echo"<br><br>";
+
+
+
+
+?>
